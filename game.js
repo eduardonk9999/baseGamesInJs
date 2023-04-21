b2e502b052cd381b96877bb04dc601dac090b515
eduardonk9999 <thiago.novak7@gmail.com>
2023-04-21 12:54:11 -0300

    feat: incluindo numeracao de cada quadrado do vetor

--- game.js
+++ game.js
@@ -7,15 +7,15 @@ window.onload = function() {
   
 
   for(var LARGURAX = 0; LARGURAX < 20; LARGURAX++) {
-    console.log(LARGURAX)
-    
     for(var ALTURAY = 0; ALTURAY < 20; ALTURAY++) {
       quadrado.fillStyle = "gray"
       quadrado.fillRect(ALTURAY * 20, LARGURAX * 20, tela.width, tela.height)
       quadrado.strokeStyle = "black";
       quadrado.strokeRect(ALTURAY * 20, LARGURAX * 20, tela.width, tela.height)
 
-      
+      //quadrado.font = "16px Arial"
+      //quadrado.fillStyle = "white"
+      //quadrado.fillText(`${[ALTURAY]}`, ALTURAY * 20,  LARGURAX * 20);
       
     
     }
